c00c3fda97c3b42ccf11b9a333680179c5787fd9
Sketch <siteshkjha9@gmail.com>
2024-08-31 17:58:23 +0530

    Learn about servlet and client server connection by making form

---
+++ src/java/registration.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class registration extends HttpServlet
+{
+    public void doPost(HttpServletRequest req,
+            HttpServletResponse res) throws IOException, 
+            ServletException
+    {
+        res.setContentType("text/html");
+        PrintWriter pw1= res.getWriter();
+        String Email=req.getParameter("n1");
+        String Pass = req.getParameter("n2");
+        String Name=req.getParameter("n3");
+        String Answer = req.getParameter("n5");
+        String Contact = req.getParameter("n6");
+        
+        
+        pw1.println
+                ("<html><body bgcolor=skyblue>Your Email is "+Email+"<br>Your Password is "+Pass+"<br>Your Name is "+Name+"<br> Your Contact no. "+Contact+"</body></html>");
+    }
+}
